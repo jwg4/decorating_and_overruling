ad2277a2598078f481628534ea37d3ddffdccee6
Jack <jack.grahl@yahoo.co.uk>
2015-11-16 22:58:19 +0000

    First attempt. Does not work.

--- decorator.py
+++ decorator.py
@@ -2,5 +2,12 @@ def test_munge(a, **kwargs):
     return "foo %s bar %s baz" % (a, kwargs)
 
 def trial_decorator(f):
-    return f
+    def g(x):
+        old_munge = munge
+        munge = dict
+
+        old_ret = f(x)
+        z = dict(w="wozzat", k="keyber")
+        return old_munge(old_ret, **z)
+    return g
 
