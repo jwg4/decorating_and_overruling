b185f4003e0e5b2e3be92d408c331f9ed73d4e9b
Jack Grahl <jack.grahl@gmail.com>
2018-04-02 22:58:32 +0000

    Get the script to run correctly and fail if not.

--- tools/run_md_flake8.sh
+++ tools/run_md_flake8.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 
 mkdir stripped
 
 for i in [0-9]{2}_*.py
 do
     d=`echo $i | sed 's/md$/py/'`
-    python tools/strip_code $i stripped/$d
+    python tools/strip_code.py $i stripped/$d
 done
 
 flake8 stripped/
