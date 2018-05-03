4ce54de5014a2cefc96d51ccd15c7b4dd5b5314c
jwg4 <jwg4@users.noreply.github.com>
2018-05-03 16:09:29 +0000

    Add an example of how to do this.

--- 73_call_attribute.py
+++ 73_call_attribute.py
@@ -1,1 +1,21 @@
 Many Python objects have an attribute called '__call__', which is what is needed to use the 'brackets' operator on a thing.
+
+>>> def fn(x):
+...     return 4
+
+>>> class Foo(object):
+...     pass
+
+>>> foo = Foo()
+>>> foo()
+Traceback (most recent call last):
+  ...
+TypeError: 'Foo' object is not callable
+
+>>> class Foo(object):
+...     __call__ = fn
+
+>>> foo = Foo()
+>>> foo()
+4
+
